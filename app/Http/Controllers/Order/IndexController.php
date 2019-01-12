e74f35f7fde88a75f4a74001f51dfd40ccc3768c
adminshan <2644586064@qq.com>
2019-01-12 08:37:09 +0800

    order

--- app/Http/Controllers/Order/IndexController.php
+++ app/Http/Controllers/Order/IndexController.php
@@ -121,7 +121,7 @@ class indexController extends Controller
         }
     }
     public function test(){
-        $url="http://ordershan.com";
+        $url="http://www.ordershan.com";
         $client=new Client(['base_uri'=>$url,'timeout'=>2.0,]);
         $response=$client->request('GET','/index.php');
         echo $response->getBody();
